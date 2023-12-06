389a07789cb2ce20550a66e4d58b43f47642921f
Louis Moses <louismoses.rait@gmail.com>
2023-12-06 22:29:04 +0800

    add navbar and logo

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -16,8 +16,22 @@ let showCreatePost = ref(false);
 </script>
 
 <template>
-    <div id="MainLayout" class="bg-red-600">
-        <div>MainLayout</div>
+    <div id="MainLayout" class="w-full h-screen">
+        <div
+            v-if="$page.url === '/'"
+            id="TopNavHome"
+            class="fixed z-30 md:hidden block w-full bg-white h-[61px] border-b border-b-gray-300"
+        >
+            <div class="flex items-center justify-between h-full">
+                <Link href="/">
+                    <img
+                        class="w-[105px] ml-6 cursor-pointer"
+                        src="/insta-logo.png"
+                        alt="Instagram Logo"
+                    />
+                </Link>
+            </div>
+        </div>
     </div>
     <slot></slot>
 </template>
