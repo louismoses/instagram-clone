e8705f36b0a35534109eb7d760691ff6ab958006
Louis Moses <louismoses.rait@gmail.com>
2023-12-06 21:52:36 +0800

    initial setup

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainLayout from "@/Layouts/MainLayout.vue";
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <MainLayout>
+        <div class="bg-green-500">Home.Vue</div>
+    </MainLayout>
+</template>
