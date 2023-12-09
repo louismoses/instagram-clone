b68211fe377c79bd37ae989b05652155ae0ec58b
Louis Moses <louismoses.rait@gmail.com>
2023-12-09 11:39:49 +0800

    add Home Page inside layout

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -4,13 +4,10 @@ import { Head, Link } from "@inertiajs/vue3";
 
 import Magnify from "vue-material-design-icons/Magnify.vue";
 import HeartOutline from "vue-material-design-icons/HeartOutline.vue";
-import HomeOutline from "vue-material-design-icons/HomeOutline.vue";
-import Compass from "vue-material-design-icons/Compass.vue";
-import SendOutline from "vue-material-design-icons/SendOutline.vue";
-import Plus from "vue-material-design-icons/Plus.vue";
-import AccountOutline from "vue-material-design-icons/AccountOutline.vue";
 import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
 import AccountPlusOutline from "vue-material-design-icons/AccountPlusOutline.vue";
+
+import MenuItem from "@/Components/MenuItem.vue";
 
 let showCreatePost = ref(false);
 </script>
@@ -49,7 +46,73 @@ let showCreatePost = ref(false);
                 </div>
             </div>
         </div>
-        <div v-if="$page.url !== '/'" id="TopNavUser"></div>
+        <div
+            v-if="$page.url !== '/'"
+            id="TopNavUser"
+            class="md:hidden fixed flex items-center justify-center z-30 w-full bg-white h-[61px] border-b border-b-gray-300"
+        >
+            <Link href="/" class="px-4">
+                <ChevronLeft :size="30" class="cursor-poiter"
+            /></Link>
+            <div class="font-extrabold text-lg">NAME HERE</div>
+            <AccountPlusOutline :size="30" class="cursore-pointer px-4" />
+        </div>
+
+        <div
+            id="SideNav"
+            class="fixed h-full bg-white xl:w-[280px] w-[80px] md:block hidden border-r border-r-gray-300"
+        >
+            <Link href="/">
+                <img
+                    src="/insta-logo-small.png"
+                    class="xl:hidden block w-[25px] mt-10 ml-[25px] mb-10 cursor-pointer"
+                />
+                <img
+                    src="/insta-logo.png"
+                    class="xl:block hidden w-[120px] mt-10 ml-6 mb-10 cursor-pointer"
+                />
+            </Link>
+
+            <div class="px-3">
+                <Link href="/">
+                    <MenuItem iconString="Home" class="mb-4" />
+                </Link>
+                <Link href="/">
+                    <MenuItem iconString="Search" class="mb-4" />
+                </Link>
+                <Link href="/">
+                    <MenuItem iconString="Explore" class="mb-4" />
+                </Link>
+                <Link href="/">
+                    <MenuItem iconString="Message" class="mb-4" />
+                </Link>
+                <Link href="/">
+                    <MenuItem iconString="Notification" class="mb-4" />
+                </Link>
+                <Link href="/">
+                    <MenuItem iconString="Create" class="mb-4" />
+                </Link>
+                <Link href="/">
+                    <MenuItem iconString="Profile" class="mb-4" />
+                </Link>
+                <Link href="/">
+                    <MenuItem iconString="Log out" class="mb-4" />
+                </Link>
+            </div>
+        </div>
+        <div
+            class="flex lg:justify-between bg-white h-full w-[100%-280px] xl:pl-[280px] lg:pl-[100px] overflow-auto"
+        >
+            <div
+                class="mx-auto md:pt-6 pt-20"
+                :class="
+                    $page.url === '/' ? 'lg:w-8/12 w-full' : 'max-w-[1200px]'
+                "
+            >
+                <main>
+                    <slot></slot>
+                </main>
+            </div>
+        </div>
     </div>
-    <slot></slot>
 </template>
