8d782897312ede1a4234612e4839c50c63f1b0ef
Louis Moses <louismoses.rait@gmail.com>
2023-12-10 22:43:05 +0800

    user page

---
+++ resources/js/Pages/User.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { reactive, toRefs } from "vue";
+import { Head, Link, router } from "@inertiajs/vue3";
+import MainLayout from "@/Layouts/MainLayout.vue";
+
+let data = reactive({ post: null });
+const form = reactive({ file: null });
+
+const props = defineProps({ postByUser: Object, user: Object });
+const { postByUser, user } = toRefs;
+</script>
+
+<template>
+    <!-- why is head not working? hmmm -->
+    <Head title="USER" />
+    <MainLayout>
+        <div class="bg-red-500">user.Vue</div>
+    </MainLayout>
+</template>
