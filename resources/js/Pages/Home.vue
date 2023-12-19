869414988ba8ea8bc67669cca0fb163e1195ca47
Louis Moses <louismoses.rait@gmail.com>
2023-12-19 19:35:47 +0800

    update home page 40:11

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,10 @@
 <script setup>
-import { Head } from "@inertiajs/vue3";
+import { ref, onMounted, toRefs } from "vue";
+import { Head, Link, router } from "@inertiajs/vue3";
 import MainLayout from "@/Layouts/MainLayout.vue";
+
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Navigation } from "vue3-carousel";
 </script>
 
 <template>
