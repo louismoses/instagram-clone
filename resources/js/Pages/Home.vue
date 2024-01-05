cb31c6585fe7bc785dff4532d1d195ecefc4746a
Louis Moses <louismoses.rait@gmail.com>
2024-01-05 19:59:34 +0800

    add user carousel 44:48

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,12 +5,62 @@ import MainLayout from "@/Layouts/MainLayout.vue";
 
 import "vue3-carousel/dist/carousel.css";
 import { Carousel, Slide, Navigation } from "vue3-carousel";
+
+import DotsHorizontal from "vue-material-design-icons/DotsHorizontal.vue";
+
+let wWidth = ref(window.innerWidth);
+let currentSlide = ref(0);
+let currentPost = ref(null);
+let openOverlay = ref(false);
+
+const props = defineProps({ post: Object, allUsers: Object });
+const { post, allUsers } = toRefs(props);
+
+onMounted(() => {
+    window.addEventListener("resize", () => {
+        wWidth.value = window.innerWidth;
+    });
+});
 </script>
 
 <template>
     <!-- why is head not working? hmmm -->
-    <Head title="HOME" />
+    <Head title="Instagram" />
     <MainLayout>
-        <div class="bg-green-500">Home.Vue</div>
+        <div class="mx-auto lg:pl-0 md:pl-[80px] pl-0">
+            <Carousel
+                v-model="currentSlide"
+                class="max-w-[800px] mx-auto"
+                :items-to-show="wWidth >= 768 ? 10 : 6"
+                :items-to-scroll="4"
+                :wrap-around="true"
+                :transition="500"
+                snap-align="start"
+            >
+                <Slide v-for="slide in 10" :key="slide">
+                    <Link
+                        href="/"
+                        class="relative mx-auto text-center mt-4 px-2 cursor-pointer"
+                    >
+                        <div
+                            class="absolute z-[-1] -top-[5px] left-[4px] rounded-full rotate-45 w-[64px] h-[64px] contrast-[1.3] bg-gradient-to-t from-yellow-300 to-purple-500 via-red-500"
+                        >
+                            <div
+                                class="rounded-full ml-[3px] mt-[3px] w-[58px] h-[58px] bg-white"
+                            ></div>
+                        </div>
+                        <img
+                            class="rounded-full w-[56px] h-[56px] -mt-[1px]"
+                            src="https://picsum.photos/id/50/300/320"
+                        />
+                        <div
+                            class="text-sx mt-3 w-[60px] truncate text-ellipsis overflow-hidden"
+                        >
+                            Name Here
+                        </div>
+                    </Link>
+                </Slide>
+            </Carousel>
+        </div>
     </MainLayout>
 </template>
